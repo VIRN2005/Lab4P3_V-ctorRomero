4f89b1874c064d133813183ea8c1ed16df054646
Víctor Romero <virn2005@gmail.com>
2023-05-12 15:45:09 -0600

    Ejercicio 2... Listo

--- Lab4P3_Víctor_Romero.cpp
+++ Lab4P3_Víctor_Romero.cpp
@@ -60,6 +60,87 @@ int Ejercicio_1() {
 	return 0;
 }
 
+//Ejercicio 2
+int num_repeticiones(char c, const string& palabra) {
+	int cont = 0;
+	for (char letra : palabra) {
+		if (letra == c) {
+			cont++;
+		}
+	}
+	return cont;
+}
+
+string buscar_secuencia(const string& cadena) {
+	string secuencia_repetida;
+	string secuencia_actual;
+
+	for (char c : cadena) {
+		if (secuencia_actual.empty() || c == secuencia_actual.back()) {
+			secuencia_actual += c;
+		}
+		else {
+			if (secuencia_actual.length() > secuencia_repetida.length()) {
+				secuencia_repetida = secuencia_actual;
+			}
+			secuencia_actual = string(1, c);
+		}
+	}
+
+	if (secuencia_actual.length() > secuencia_repetida.length()) {
+		secuencia_repetida = secuencia_actual;
+	}
+
+	return secuencia_repetida;
+}
+
+void analizar_cadena_adn(const string& cadena) {
+	double total = cadena.length();
+	
+	//Valores de Adenina
+	double adenina = num_repeticiones('A', cadena);
+	double porcentaje_adenina = (adenina / total) * 100;
+
+	//Valores de Citosina
+	double citosina = num_repeticiones('C', cadena);
+	double porcentaje_citosina = (citosina / total) * 100;
+
+	//Valores de Guanina
+	double guanina = num_repeticiones('G', cadena);
+	double porcentaje_guanina = (guanina / total) * 100;
+
+	//Valores de Timina
+	double timina = num_repeticiones('T', cadena);
+	double porcentaje_timina = (timina / total) * 100;
+
+	string secuencia_repetida = buscar_secuencia(cadena);
+
+	cout << "Total de nucleótidos: " << total << endl;
+	cout << "Numero de Ocurrencias de cada Nucleotico: \n";
+	cout << "Adenina (A): " << adenina << endl;
+	cout << "Citosina (C): " << citosina << endl;
+	cout << "Guanina (G): " << guanina << endl;
+	cout << "Timina (T): " << timina << endl << endl;
+	
+	cout << "Análisis de la cadena de ADN:" << endl;
+	cout << "Adenina (A): (" << porcentaje_adenina << "%)" << endl;
+	cout << "Citosina (C): (" << porcentaje_citosina << "%)" << endl;
+	cout << "Guanina (G): (" << porcentaje_guanina << "%)" << endl;
+	cout << "Timina (T): (" << porcentaje_timina << "%)" << endl;
+	cout << "Secuencia más larga de nucleótidos consecutivos que se repiten: " << secuencia_repetida << endl << endl;
+}
+
+int Ejercicio_2() {
+	string cadena;
+
+	cout << "Ingrese una cadena de ADN: ";
+	cin >> cadena;
+
+	analizar_cadena_adn(cadena);
+
+	return 0;
+}
+
 // Menu de Opciones
 int menu() {
 	setlocale(LC_ALL, "spanish");
@@ -91,7 +172,7 @@ int menu() {
 
 		case 2: {
 			cout << "- 2) Análisis de ADN - \n";
-
+			Ejercicio_2();
 		}
 			  break;
 
